bb80e3fa62b84187e8d90036cf268598f4f13f6a
DanaJoffe <djoffe94@gmail.com>
2020-04-03 15:46:14 +0300

    chanhe board size

--- ex1/main.py
+++ ex1/main.py
@@ -2,7 +2,7 @@ from GUI import CellAutomatonGameGUI
 from logic.CellAutomatonGame import CellAutomatonGame
 
 if __name__ == '__main__':
-    gui = CellAutomatonGameGUI(CellAutomatonGame())
+    gui = CellAutomatonGameGUI(CellAutomatonGame(200, 200))
     gui.set_all()
     gui.start()
 
